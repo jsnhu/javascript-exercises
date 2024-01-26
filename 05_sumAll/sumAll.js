833be7b8fd370af42788eee653920da4fa15ae56
Jason Hu <jsnhu.os@gmail.com>
2024-01-25 23:04:40 -0800

    05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    if (typeof(firstNum) != 'number' || typeof(secondNum) != 'number') {
+        return "ERROR";
+    }
+    if (firstNum < 0 || secondNum < 0) {
+        return "ERROR";
+    }
 
+    let beginNum = firstNum;
+    let endNum = secondNum;
+
+    if (firstNum > secondNum) {
+        endNum = firstNum;
+        beginNum = secondNum;
+    }
+    let sum = 0;
+    for (i=beginNum; i<=endNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
